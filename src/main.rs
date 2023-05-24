c7fdc521a6f36f51e0c95b6b73b0bb339ade7c47
David Gaspar <davidgaspar.dev@gmail.com>
2023-05-24 06:17:21 +0100

    chore: implement production speed and diameter checks in main control loop

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,20 @@ fn main() {
     let relay_control = relay::RelayController::new(0);
     let mut s7_client = s7::S7Client::connect("192.168.0.1".to_string());
 
-    println!("read_dword: {}", s7_client.read_dword(1, 1132));
-
     loop {
-        s7_client.read_dword(1, 1132);
+        let production_speed = s7_client.read_dword(1, 1088);
+        let (diameter_x, diameter_y) = s7_client.read_double_dword(1, 1132);
 
-        relay_control.turn_on();
-        sleep(Duration::from_secs(10));
-        relay_control.turn_off();
+        if cfg!(debug_assertions) {
+            println!("production_speed: {}", production_speed);
+            println!("diameter x: {}", diameter_x);
+            println!("diameter y: {}", diameter_y);
+        }
+
+        if production_speed > 100 && (diameter_x < 2 || diameter_y < 2) {
+            relay_control.turn_on();
+            sleep(Duration::from_secs(10));
+            relay_control.turn_off();
+        }
     }
 }
