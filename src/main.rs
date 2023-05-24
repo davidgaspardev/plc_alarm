716a6abda0e3cb03f586b088ac5e8fa8a6db8e51
David Gaspar <davidgaspar.dev@gmail.com>
2023-05-24 06:20:56 +0100

    feat: add command-line argument for IP address in main function

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
 mod relay;
 mod s7;
 
+use std::env;
 use std::thread::sleep;
 use std::time::Duration;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        panic!("Please provide the IP address as a command line argument");
+    }
+
+    let ip_address = &args[1];
+    let mut s7_client = s7::S7Client::connect(ip_address.to_string());
+
     let relay_control = relay::RelayController::new(0);
-    let mut s7_client = s7::S7Client::connect("192.168.0.1".to_string());
 
     loop {
         let production_speed = s7_client.read_dword(1, 1088);
