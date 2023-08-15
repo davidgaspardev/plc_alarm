7e3eb4c0fa9e94cfcaed2f985763933443e48e69
David Gaspar <david.correa@intelbras.com.br>
2023-08-14 22:06:44 -0300

    refactor: set varible for UPPERCASE

--- setup.sh
+++ setup.sh
@@ -11,53 +11,54 @@ else
     sudo apt install -y libudev-dev
 fi
 
-read -p "Enter PLC ip to listener: " plc_address
+SERVICE_PATH=/etc/systemd/system/plc_alarm.service
+BINARY_PATH=/usr/local/bin/plc_alarm
+WORK_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-service_path=/etc/systemd/system/plc_alarm.service
-binary_path=/usr/local/bin/plc_alarm
-working_directory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-cd $working_directory
+# Change directory to work directory
+cd $WORK_DIR
 
 # Check if the binary file already exists
-if [ -f $binary_path ]; then
-    read -p "$binary_path already exists. Make a new build? (y/n): " choice
+if [ -f $BINARY_PATH ]; then
+    read -p "$BINARY_PATH already exists. Make a new build? (y/n): " choice
     if [[ $choice != "y" && $choice != "Y" ]]; then
         echo "Continue with build current."
     else
         cargo build --release
-        sudo cp -f target/release/plc_alarm $binary_path
+        sudo cp -f target/release/plc_alarm $BINARY_PATH
     fi
 else
     cargo build --release
-    sudo cp target/release/plc_alarm $binary_path
+    sudo cp target/release/plc_alarm $BINARY_PATH
 fi
 
 # Check if the service file already exists
-if [ -f $service_path ]; then
-    read -p "$service_path already exists. Overwrite? (y/n): " choice
+if [ -f $SERVICE_PATH ]; then
+    read -p "$SERVICE_PATH already exists. Overwrite? (y/n): " choice
     if [[ $choice != "y" && $choice != "Y" ]]; then
         echo "Exiting without overwriting."
         exit 1
     fi
 fi
 
+read -p "Enter PLC ip to listener: " PLC_ADDRESS
+
 # Create the service file content
-cat <<EOL | sudo tee $service_path
+cat <<EOL | sudo tee $SERVICE_PATH
 [Unit]
 Description=PLC Alarm for Tubarão Intelbras
 After=network-online.target
 Wants=network-online.target
 
 [Service]
-ExecStart=/bin/bash -c '$binary_path $plc_address >> $working_directory/output.csv 2>> $working_directory/error.log'
+ExecStart=/bin/bash -c '$BINARY_PATH $PLC_ADDRESS >> $WORK_DIR/output.csv 2>> $WORK_DIR/error.log'
 Restart=always
 User=root
 Group=root
 Environment=PATH=/usr/bin:/usr/local/bin
-WorkingDirectory=$working_directory
-# StandardOutput=file:$working_directory/output.csv
-# StandardError=file:$working_directory/error.log
+WorkingDirectory=$WORK_DIR
+# StandardOutput=file:$WORK_DIR/output.csv
+# StandardError=file:$WORK_DIR/error.log
 RestartSec=3
 LimitNOFILE=4096
 
@@ -65,8 +66,8 @@ LimitNOFILE=4096
 WantedBy=multi-user.target
 EOL
 
-touch $working_directory/output.log
-touch $working_directory/error.log
+touch $WORK_DIR/output.log
+touch $WORK_DIR/error.log
 
 # Reload systemd
 sudo systemctl daemon-reload
